b75a73c2ffa791a961689a3032fa8db7ba5e066c
tharoon <tharoon.raj@gmail.com>
2020-07-25 00:16:28 -0500

    Updates

---
+++ precache-manifest.10ee9942752749c8c8ee41c62a26b3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a02868973ced73b85e30e08a7706688",
+    "url": "/COVID19Tracker/index.html"
+  },
+  {
+    "revision": "a7fee4fbddd0f4f61ced",
+    "url": "/COVID19Tracker/static/css/main.7591ea91.chunk.css"
+  },
+  {
+    "revision": "fe216cdde85f76a25b9e",
+    "url": "/COVID19Tracker/static/js/2.388ab532.chunk.js"
+  },
+  {
+    "revision": "cef050e2d281d95590dbc30c1306f491",
+    "url": "/COVID19Tracker/static/js/2.388ab532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fee4fbddd0f4f61ced",
+    "url": "/COVID19Tracker/static/js/main.1b18f46e.chunk.js"
+  },
+  {
+    "revision": "8f680cb71f4d8e9d9c73",
+    "url": "/COVID19Tracker/static/js/runtime-main.431e9960.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19Tracker/static/media/image.d7265326.png"
+  }
+]);
